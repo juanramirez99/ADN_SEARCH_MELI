db8da177e38e42a6c92d40da0b99c0758eda34b0
Juan Ramirez <Juan.ramirez@otrcapital.com>
2022-06-09 23:19:46 -0500

    inject DNa processing service

--- LEVEL2_ADN_PROCESSING_API/Controllers/ADNProcessingController.cs
+++ LEVEL2_ADN_PROCESSING_API/Controllers/ADNProcessingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LEVEL2_ADN_PROCESSING_API.Service;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 
@@ -9,28 +10,47 @@ namespace LEVEL2_ADN_PROCESSING_API.Controllers
     public class ADNProcessingController : ControllerBase
     {
         private readonly ILogger<ADNProcessingController> _logger;
+        private readonly DNAProcessingService _processingService;
 
-        public ADNProcessingController(ILogger<ADNProcessingController> logger)
+        public ADNProcessingController(ILogger<ADNProcessingController> logger, DNAProcessingService dnaProcessingService)
         {
             _logger = logger;
+            _processingService = dnaProcessingService;
         }
 
         [HttpPost(Name = "Mutant")]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<bool>> Post()
+        public async Task<ActionResult<bool>> IsMutant(List<string> val)
         {
-            short i = 1;
-            if (i>0)
+            var x = CastToDNAMatrix(val);
+            int k = await _processingService.IsMutant(x);
+            if (k >= 2)
             {
-                return true;
+                return (true);
             }
             else
                 return NotFound(false);
         }
 
+        private static string [,] CastToDNAMatrix(List<string> val)
+        {
+            int len = val[0].Length;
+            string[,] dna = new string[len, len];
+            for (int i = 0; i < len; i++)
+            {
+                var res = val[i].Select(x => new string(x, 1)).ToArray();
+                for (int j = 0; j < len; j++)
+                {
+                    dna[i, j] = res[j];
+
+                }
+            }
+            return dna;
+        }
+
         [HttpGet(Name = "Stats")]
-        public Stats Get()
+        public Stats Stats()
         {
             Stats stats = new Stats()
             {
---
+++ LEVEL2_ADN_PROCESSING_API/Service/DNAProcessingService.cs
@@ -0,0 +1,100 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LEVEL2_ADN_PROCESSING_API.Service
+{
+    public class DNAProcessingService
+    {
+        public DNAProcessingService()
+        {
+
+        }
+
+        private int HorizontalRevision(string[,] dna, int count)
+        {
+            int len = dna.GetLength(0);
+
+            for (int i = 0; i < len; i++)
+            {
+                string tmp = string.Empty;
+                for (int j = 0; j < len; j++)
+                {
+                    tmp += dna[i, j];
+                }
+
+                if (tmp.Length > 3)
+                {
+                    count += Regex.Matches(tmp, "AAAA").Count
+                            + Regex.Matches(tmp, "TTTT").Count
+                            + Regex.Matches(tmp, "CCCC").Count
+                            + Regex.Matches(tmp, "GGGG").Count;
+                }
+
+                if (count == 2)
+                    return Convert.ToInt16(count);
+            }
+            return count;
+        }
+
+        private int VerticalRevision(string[,] dna, int count)
+        {
+            int len = dna.GetLength(0);
+            for (int j = 0; j < len; j++)
+            {
+                string tmp = string.Empty;
+                for (int i = 0; i < len; i++)
+                {
+                    tmp += dna[i, j];
+                }
+
+                if (tmp.Length > 3)
+                {
+                    count += Regex.Matches(tmp, "AAAA").Count
+                            + Regex.Matches(tmp, "TTTT").Count
+                            + Regex.Matches(tmp, "CCCC").Count
+                            + Regex.Matches(tmp, "GGGG").Count;
+                }
+                if (count == 2)
+                    return count;
+            }
+            return count;
+        }
+
+        private int ObliqueRevision(string[,] dna, int count)
+        {
+
+            int len = dna.GetLength(0);
+            for (int n = -len; n <= len - 4; n++)
+            {
+                string tmp = string.Empty;
+                for (int i = 0; i < len; i++)
+                {
+                    if ((i - n >= 0) && (i - n < len))
+                    {
+                        tmp = tmp + dna[i, (i - n)];
+                    }
+                }
+                if (tmp.Length > 3)
+                {
+                    count += Regex.Matches(tmp, "AAAA").Count
+                            + Regex.Matches(tmp, "TTTT").Count
+                            + Regex.Matches(tmp, "CCCC").Count
+                            + Regex.Matches(tmp, "GGGG").Count;
+                }
+                if (count == 2)
+                    return count;
+            }
+            return count;
+        }
+
+        public async Task<int> IsMutant(string[,] dna)
+        {
+            Task<int> sequenceCount = 0;
+            sequenceCount = await Task.Run(() => VerticalRevision(dna, sequenceCount)).Result;
+            if (sequenceCount < 2)
+                sequenceCount = await Task.Run(() => HorizontalRevision(dna, sequenceCount)).Result;
+            if (sequenceCount < 2)
+                sequenceCount = await Task.Run(() => ObliqueRevision(dna, sequenceCount)).Result;
+            return sequenceCount;
+        }
+    }
+}
